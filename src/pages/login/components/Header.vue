c600282550beb2ebb1f70f755d046aedb76444e1
huyanhai <810839700@qq.com>
2022-06-13 21:29:28 +0800

    feat: add

---
+++ src/pages/login/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="login-header">
+    <logo-full-icon class="logo" />
+    <div class="operations-container">
+      <t-button theme="default" shape="square" variant="text">
+        <t-icon name="help-circle" class="icon" />
+      </t-button>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import LogoFullIcon from '@/assets/assets-logo-full.svg?component';
+</script>
+
+<style lang="less" scoped>
+@import '@/style/variables.less';
+.login-header {
+  height: 64px;
+  padding: 0 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  backdrop-filter: blur(5px);
+  color: var(--tdvns-text-color-primary);
+
+  .logo {
+    width: 188px;
+    height: 64px;
+  }
+
+  .operations-container {
+    display: flex;
+    align-items: center;
+    .t-button {
+      margin-left: 16px;
+    }
+
+    .icon {
+      height: 20px;
+      width: 20px;
+      padding: 6px;
+      box-sizing: content-box;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login-wrapper">
+    <login-header />
+
+    <div class="login-container">
+      <div class="title-container">
+        <h1 class="title margin-no">登录到管理台</h1>
+      </div>
+
+      <login v-if="type === 'login'" />
+      <register v-else @register-success="switchType('login')" />
+      <tdesign-setting />
+    </div>
+
+    <footer class="copyright">Copyright @ 2021-2022 xxx. All Rights Reserved</footer>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'LoginIndex',
+};
+</script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import Login from './components/Login.vue';
+import Register from './components/Register.vue';
+import LoginHeader from './components/Header.vue';
+import TdesignSetting from '@/layouts/setting.vue';
+
+const type = ref('login');
+const switchType = (val: string) => {
+  type.value = val;
+};
+</script>
+
+<style lang="less" scoped>
+@import url('./index.less');
+</style>
